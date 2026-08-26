8a2e21f4402a86eed6c201e085889935e0944bbb
mizuki <6821901016@cdti.ac.th>
2026-08-26 09:45:57 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import BlogCard from "@/components/BlogCard";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
-      <h1>Welcome to My App</h1>
+    <div className="container">
+
+      <BlogCard/>
+
+      
     </div>
   );
 }
